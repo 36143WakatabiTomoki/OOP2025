a53391fcd2667ae209ba98c6d4aee039ceb6d274
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-04-23 10:27:27 +0900

    ヤードに変換

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -3,8 +3,8 @@
         static void Main(string[] args) {
 
             Console.WriteLine("***　変換アプリ　***");
-            Console.WriteLine("１：インチからメートル");
-            Console.WriteLine("２：メートルからインチ");
+            Console.WriteLine("１：ヤードからメートル");
+            Console.WriteLine("２：メートルからヤード");
             Console.Write("＞");
             int check = int.Parse(Console.ReadLine());
 
@@ -16,15 +16,19 @@
 
 
             if (check == 1) {
-                PrintInchToMeterList(start, end);
+                PrintYardToMeterList(start, end);
             } else if (check == 2) {
-                PrintMeterToInchList(start, end);
+                PrintMeterToYardList(start, end);
+            /*} else if (check == 3) {
+                PrintInchToMeterList(start, end);
+            } else if (check == 4) {
+                PrintMeterToInchList(start, end);*/
             } else {
                 Console.WriteLine("Error");
             }
         }
 
-
+        /*
         // インチからメートルへの対応表を出力
         static void PrintInchToMeterList(int start, int end) {
             for (int Inch = start; Inch <= end; Inch++) {
@@ -39,9 +43,25 @@
                 double inch = ToInch(Meter);
                 Console.WriteLine($"{Meter} m = {inch:0.0000} inch");
             }
+        }*/
+
+        // ヤードからメートルへの対応表を出力
+        static void PrintYardToMeterList(int start, int end) {
+            for (int Yard = start; Yard <= end; Yard++) {
+                double meter = FromYard(Yard);
+                Console.WriteLine($"{Yard} yard = {meter:0.000} m");
+            }
         }
 
+        // メートルからヤードへの対応表を出力
+        static void PrintMeterToYardList(int start, int end) {
+            for (int Meter = start; Meter <= end; Meter++) {
+                double yard = ToYard(Meter);
+                Console.WriteLine($"{Meter} m = {yard:0.000} yard");
+            }
+        }
 
+        /*
         // インチからメートルを求める
         public static double FromInch(double Inch) {
             return Inch * 0.0254;
@@ -50,6 +70,16 @@
         // メートルからインチを求める
         public static double ToInch(double Meter) {
             return Meter / 0.0254;
+        }*/
+
+        // ヤードからメートルを求める
+        public static double FromYard(double Yard) {
+            return Yard * 0.9144;
+        }
+
+        // メートルからヤードを求める
+        public static double ToYard(double Meter) {
+            return Meter / 0.9144;
         }
     }
 }
