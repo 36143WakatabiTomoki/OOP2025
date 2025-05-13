40f6bc249c23aab7fc609c3d1b02dd0f12f5a248
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-05-13 10:22:11 +0900

    2.1.2 終了

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -2,7 +2,7 @@
     internal class Program {
         static void Main(string[] args) {
             var sales = new SalesCounter(@"data\sales.csv");
-            IDictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
+            var amountsPerStore = sales.GetPerStoreSales();
             foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
