9383ac6df52beed181aed4d020c1d4f337c2b2ad
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-05-16 14:14:30 +0900

    P68まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,7 +7,7 @@
             
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            Console.WriteLine(Count(numbers, delegate(int n) { return n % 2 == 0; }));
+            Console.WriteLine(Count(numbers, n => n >= 5 && n < 10));
         }
 
         static int Count(int[] numbers, Predicate<int> judge) {
