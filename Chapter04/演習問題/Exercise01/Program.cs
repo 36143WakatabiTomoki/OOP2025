981851937ff1bfdaeb594294b6e765c0f584caf0
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-05-23 15:24:33 +0900

    4.2.1 完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -15,13 +15,44 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
+            foreach (var n in langs) {
+                if(n.Contains('S')) {
+                    Console.WriteLine(n);
+                }
+            }
 
+            Console.WriteLine();
+
+            for (int count = 0; count < langs.Count; count++) {
+                if (langs[count].Contains('S')) {
+                    Console.WriteLine(langs[count]);
+                }
+            }
+
+            Console.WriteLine();
+
+            while (true) {
+                foreach (var n in langs) {
+                    if (n.Contains('S')) {
+                        Console.WriteLine(n);
+                    }
+                }
+                break;
+            }
+
+            Console.WriteLine();
         }
 
         private static void Exercise2(List<string> langs) {
+            var name = langs.Where(n => n.Contains('S')).ToArray();
+            foreach (var select in name) {
+                Console.WriteLine(select);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
+            var lang = langs.Find(n => n.Length == 10);
+            Console.WriteLine(lang ??= "unknown");
         }
     }
 }
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            var number = int.Parse(Console.ReadLine());
+            if(number < 0 || number >= 500) {
+                Console.WriteLine(number);
+            }else if(number < 100) {
+                Console.WriteLine(number * 2);
+            }else if(number < 500) {
+                Console.WriteLine(number * 3);
+            }else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
+        }
+
+        private static void Exercise2() {
+            var number = int.Parse(Console.ReadLine());
+            switch (number) {
+                case < 0 or >= 500:
+                    Console.WriteLine(number);
+                    break;
+                case < 100:
+                    Console.WriteLine(number * 2);
+                    break;
+                case < 500:
+                    Console.WriteLine(number * 3);
+                    break;
+                case :
+                    Console.WriteLine("入力値に誤りがあります");
+                    break;
+            }
+        }
+
+        private static void Exercise3() {
+        }
+    }
+}
