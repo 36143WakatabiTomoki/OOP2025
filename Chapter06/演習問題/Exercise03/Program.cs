7145ba6e5d2856613463bb4c7f2700cc89ddd8a9
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-06-04 10:00:38 +0900

    6.6 完成 1

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace Exercise03 {
     internal class Program {
@@ -20,7 +22,11 @@ namespace Exercise03 {
             Console.WriteLine("6.3.5");
             Exercise5(text);
 
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+
         }
+
 
         private static void Exercise1(string text) {
             //var space = text.Count(t => text.Contains(' '));
@@ -55,6 +61,26 @@ namespace Exercise03 {
                 if (word.Length <= 4) {
                     Console.WriteLine(word);
                 }
+            }
+        }
+
+        private static void Exercise6(string text) {
+            Console.WriteLine("文字列：" + text);
+            Console.WriteLine();
+            Console.WriteLine("出現アルファベットの個数");
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var cultureInfo = new CultureInfo("ja-JP");
+            int count = 0;
+            foreach (var alphabetsolo in alphabet) {
+                String alphabetString = alphabetsolo.ToString();
+                foreach (var word in text) {
+                    String wordString = word.ToString();
+                    if (String.Compare(wordString, alphabetString, cultureInfo, CompareOptions.IgnoreCase) == 0) {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"{alphabetsolo}：{count}");
+                count = 0;
             }
         }
     }
