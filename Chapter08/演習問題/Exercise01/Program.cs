1e5be1598b6da5caaed231d9f57139d8324a5563
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-06-11 11:02:23 +0900

    8.1.1 終了

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var textDict = new Dictionary<Char, int>();
+            foreach (var ch in text.ToUpper()) {
+                if('A' <= ch && ch <= 'Z') {
+                    if(textDict.ContainsKey(ch)) {
+                        textDict[ch]++;
+                    } else {
+                        textDict.Add(ch, 1);
+                    }
+                }
+            }
+            var dict = textDict.OrderBy(d => d.Key).ToDictionary();
+            foreach (var (key, value) in dict) {
+                Console.WriteLine($"{key}：{value}");
+            }
+        }
+
+        private static void Exercise2(string text) {
+        }
+    }
+}
