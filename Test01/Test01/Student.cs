4f0bf762afc95144d6221f868c8fd650be4fc906
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-06-17 11:12:04 +0900

    設問１_Student クラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string? Name { get; set; }
+        public string? Subject { get; set; }
+        public int Score { get; set; }
+
+
+
+    }
+}
