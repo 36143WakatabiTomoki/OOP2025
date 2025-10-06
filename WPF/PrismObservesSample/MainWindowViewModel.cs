185350e044fa37fce82d612b64c0b5d933a835c5
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-10-06 15:07:31 +0900

    足し算の処理

---
+++ WPF/PrismObservesSample/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismObservesSample
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _input1 = "";
+        public string Input1 {
+            get => _input1;
+            set => SetProperty(ref _input1, value);
+        }
+
+        private string _input2 = "";
+        public string Input2 {
+            get => _input2;
+            set => SetProperty(ref _input2, value);
+        }
+
+        private string _result = "";
+        public string Result {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        //コンストラクタ
+        public MainWindowViewModel() {
+            SumCommand = new DelegateCommand(ExcuteSum);
+        }
+        public DelegateCommand SumCommand { get; }
+
+        //足し算の処理
+        private void ExcuteSum() {
+            int sum = int.Parse(_input1) + int.Parse(_input2);
+            Result = sum.ToString();
+        }
+    }
+}
