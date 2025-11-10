0d9438b52278cead5284fff7f168e9e7de455d53
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 13:45:52 +0900

    問10.1_2 終了

--- Chapter10/演習問題/Exercise01_1/Program.cs
+++ Chapter10/演習問題/Exercise01_1/Program.cs
@@ -2,21 +2,36 @@
     internal class Program {
         static void Main(string[] args) {
             var readTextPath = "Library.cs";
-            CountClass(readTextPath);
+            Console.WriteLine(CountClass(readTextPath));
         }
 
-        public static void CountClass(string readTextName) {
+        //10.1_1
+        //public static int CountClass(string readTextName) {
+        //    int count = 0;
+        //    if (File.Exists(readTextName)) {
+        //        using var readText = new StreamReader(readTextName);
+        //        while (!readText.EndOfStream) {
+        //            var line = readText.ReadLine();
+        //            if (line.Contains("class")) {
+        //                count++;
+        //            }
+        //        }
+        //    }
+        //    return count;
+        //}
+
+        //10.1_2
+        public static int CountClass(string readTextName) {
             int count = 0;
-            if (File.Exists(readTextName)) {
-                using var readText = new StreamReader(readTextName);
-                while (!readText.EndOfStream) {
-                    var line = readText.ReadLine();
-                    if (line.Contains("class")) {
-                        count++;
-                    }
+            var readText = File.ReadAllLines(readTextName);
+            foreach (var line in readText) {
+                if(line.Contains("class")) {
+                    count++;
                 }
-                Console.WriteLine(count);
             }
+            return count;
         }
+
+        //10.1_3
     }
 }
