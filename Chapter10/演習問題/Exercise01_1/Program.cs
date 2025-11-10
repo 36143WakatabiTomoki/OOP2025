ef2d3c43aa7db9c704f53a9e2b6473aba4697352
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 13:39:11 +0900

    問10.1_1 終了

---
+++ Chapter10/演習問題/Exercise01_1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise01_1 {
+    internal class Program {
+        static void Main(string[] args) {
+            var readTextPath = "Library.cs";
+            CountClass(readTextPath);
+        }
+
+        public static void CountClass(string readTextName) {
+            int count = 0;
+            if (File.Exists(readTextName)) {
+                using var readText = new StreamReader(readTextName);
+                while (!readText.EndOfStream) {
+                    var line = readText.ReadLine();
+                    if (line.Contains("class")) {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
