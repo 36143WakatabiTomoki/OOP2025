202104cb97899151755296689a1b9e22e35c67b9
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 15:21:40 +0900

    問10.4 終了 & 名前変更

--- Chapter10/演習問題/Exercise10.4/Program.cs
+++ Chapter10/演習問題/Exercise10.4/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Exercise10._4 {
+﻿namespace Exercise04 {
     internal class Program {
         static void Main(string[] args) {
-
+            var di = new DirectoryInfo("C:\\Users\\infosys\\source\\repos\\OOP2025");
+            var fileSystems = di.EnumerateFiles();
+            foreach (var item in fileSystems) {
+                if(item.Length >= 1048576) {
+                    Console.WriteLine(item.Name);
+                }
+            }
         }
     }
 }
