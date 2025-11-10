15d53badb415e096502af533025d322444ec95c5
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 14:15:37 +0900

    問10.3 終了

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var firstTextFile = Console.ReadLine();
+            Console.WriteLine();
+            var endTextFile = Console.ReadLine();
+            Console.WriteLine();
+
+            using (var writer = new StreamWriter(firstTextFile, append: true)) {
+                foreach (var line in File.ReadLines(endTextFile)) {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
