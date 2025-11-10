7f430aa6fc6cad16c4be4be2f8056ddc9e18420d
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 13:59:22 +0900

    問10.2 終了

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var readTextPath = "走れメロス.txt";
+            var writeTextPath = "writeLineText.txt";
+            int count = 1;
+            using (var writer = new StreamWriter(writeTextPath)) {
+                foreach (var line in File.ReadLines(readTextPath)) {
+                    writer.WriteLine($"{count} {line}");
+                    count++;
+                }
+            }
+        }
+    }
+}
