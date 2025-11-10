528827d8fd955eacb9bab6fb4ed3e59dc2118f04
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-10 15:57:25 +0900

    問10.5 未完成

---
+++ Chapter10/演習問題/Exercise05/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var di = new DirectoryInfo("C:\\Users\\infosys\\source\\repos\\OOP2025\\Chapter10\\演習問題\\Exercise05\\File");
+            var files = di.EnumerateFiles("*");
+            foreach (var file in files) {
+                File.Copy($"{file}", $"{file.Name}", overwrite:true);
+            }
+        }
+    }
+}
