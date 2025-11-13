bc83cab43b2ae60878c1a41fee8626c8986f161a
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2025-11-13 13:50:21 +0900

    パス指定の単語カウント

--- Chapter15/TemplateMethod/LineCounter/Program.cs
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -5,6 +5,10 @@ namespace LineCounter {
         static void Main(string[] args) {
             Console.Write("読み込むファイル：");
             string path = Console.ReadLine();
+            if (!File.Exists(path)) {
+                Console.WriteLine("ファイルが見つかりませんでした");
+                return;
+            }
             TextProcessor.Run<LineCounterProcessor>(path);
         }
     }
