98260e097238e54436d847c4af03c0de906ff298
WakatabiTomoki <oibc.36143@ojs.ac.jp>
2026-01-13 15:53:51 +0900

    卒業制作 星をみるひとAPI 使用テスト

---
+++ WPF/seisaku_test_1/seisaku_test_1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace seisaku_test_1 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            var baseAddress = new Uri("https://app.livlog.xyz/hoshimiru/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress }) {
+
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json");
+
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("authorization", "Bearer c6468419-bcf4-4f3f-ae5b-be33d524b062");
+
+                using (var response = await httpClient.GetAsync("constellation?lat=35.6581&lng=139.7414&date=2020-01-15&hour=20&min=00&id=1&disp=on")) {
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseData);
+                }
+            }
+        }
+    }
+}
